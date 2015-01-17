1babe9612438de65a8335e037810f1769efd69f8
Jacek Pajak <jacekpajak@yandex.com>
2015-01-17 17:46:27 +0100

    Ingredient show page finished

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,6 +1,6 @@
 class IngredientsController < ApplicationController
   before_action :set_ingredient, only: [:show, :edit, :update, :destroy]
-
+  layout 'main'
   # GET /ingredients
   # GET /ingredients.json
 
--- app/helpers/ingredients_helper.rb
+++ app/helpers/ingredients_helper.rb
@@ -1,2 +1,19 @@
 module IngredientsHelper
+
+def ingredient_partial
+	
+	if @ingredient.allowed == "no"
+		render 'ingredient_red'
+
+	elsif @ingredient.allowed == "warning"
+		render 'ingredient_yellow'
+	
+	elsif @ingredient.allowed == "yes"		
+		render 'ingredient_green'
+
+	end
+
+end
+	
+
 end
