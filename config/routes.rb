f14905ec374448aa7e6d58338c5bbfca263a51b5
Jacek Pajak <jacekpajak@yandex.com>
2015-01-26 10:10:14 +0100

    Add custom devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :path => '', :path_names => { :sign_in => "login", :sign_out => "logout", :sign_up => "register" }
   get '/' => 'pages#home', as: :home
   get '/faq' => 'pages#faq', as: :faq
   get '/help' => 'pages#help', as: :help
