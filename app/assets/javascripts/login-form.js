10266988b029e0bf6c757bf4179b54c2e641044b
Jacek Pajak <jacekpajak@yandex.com>
2015-02-13 22:19:08 +0100

    Finished form

--- app/assets/javascripts/login-form.js
+++ app/assets/javascripts/login-form.js
@@ -1,37 +1,57 @@
-$(function() {
+$(function(){
+
+$('#email_field, #password_field').keyup(function(){
 //Email field animation
-$('#email_field').focus( function (){
+  if ($('#email_field').val() !== "" ) {
   $('#email_label').addClass('active highlight');
-});
-
-$('#email_field').focusout( function (){
-  $('#email_label').removeClass('active highlight');
-  });
-
+}
+  else {
+    $('#email_label').removeClass('active highlight');
+  };
 //Password field animation
-$('#password_field').focus( function (){
+  if ($('#password_field').val() !== "" ) {
   $('#password_label').addClass('active highlight');
+  }
+  else {
+   $('#password_label').removeClass('active highlight'); 
+  }
+
 });
 
-$('#password_field').focusout( function (){
-  $('#password_label').removeClass('active highlight');
-  });
+$('#signup_email_field, #signup_password_field, #signup_confirmation_field').keyup(function(){
+//Signup email field animation
+  if ($('#signup_email_field').val() !== "" ) {
+  $('#signup_email_label').addClass('active highlight');
+}
+  else {
+    $('#signup_email_label').removeClass('active highlight');
+  };
+//Signup password field animation
+  if ($('#signup_password_field').val() !== "" ) {
+  $('#signup_password_label').addClass('active highlight');
+  }
+  else {
+   $('#signup_password_label').removeClass('active highlight'); 
+  }
 
+//Signup confirmation field animation
+  if ($('#signup_confirmation_field').val() !== "" ) {
+  $('#signup_confirmation_label').addClass('active highlight');
+  }
+  else {
+   $('#signup_confirmation_label').removeClass('active highlight'); 
+  }
 
+});
 
+//Tab switcher
 $('.tab a').on('click', function (e) {
-  
   e.preventDefault();
-  
   $(this).parent().addClass('active');
   $(this).parent().siblings().removeClass('active');
-  
   target = $(this).attr('href');
-
   $('.tab-content > div').not(target).hide();
-  
   $(target).fadeIn(600);
-  
 });
 
 });
