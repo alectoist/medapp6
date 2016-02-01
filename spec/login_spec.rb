2875ed310be55cd8ad8bbe8ccc7fb414b2680bb2
alecto <jacekpajak@yandex.com>
2016-02-01 19:59:22 +0100

    write some more tests

--- spec/login_spec.rb
+++ spec/login_spec.rb
@@ -3,10 +3,15 @@ require 'rails'
 require 'rspec/rails'
 require 'capybara/rails'
 
-describe "visit the login page", :type => :feature do
-  it "and expect response 200" do
-    visit '/login'
+describe "visit main without session", :type => :feature do
+  it "and expect to be redirected to /login" do
+    visit '/'    
+    expect(current_path).to eq('/login')
     expect(page.status_code).to eq(200)
   end
-end
 
+  it "and expect it to have a login form" do
+  	visit '/login' 
+  	expect(page).to have_selector('.login-wrap')
+  end
+end
